475897d06352ed29c85b9a5978da74d5f4f49e1f
kristiqn <ksdfiw@gmail.com>
2023-02-15 13:44:52 +0200

    ADD TASK 2

---
+++ Programirane/task 2/task 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagoncount = int.Parse(Console.ReadLine());
+            int [] peopleInWagons = new int[wagoncount];
+            int totalpeole = 0;
+
+            for (int i = 0; i < wagoncount; i++)
+            {
+                peopleInWagons[i]= int.Parse(Console.ReadLine());
+                totalpeole+= peopleInWagons[i];
+            }
+
+            Console.WriteLine(string.Join(" ",peopleInWagons));
+            Console.WriteLine(totalpeole);
+
+
+            
+
+        }
+    }
+}
