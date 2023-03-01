4e9c5b37f1a4431aba8a0a4372bb972395e19972
kristiqn <ksdfiw@gmail.com>
2023-03-01 13:25:58 +0200

    add

---
+++ Programirane/ConsoleApp1/convert meters to kilometres/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convert_meters_to_kilometres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double meters = double.Parse(Console.ReadLine());
+            double km = meters /1000;
+            Console.WriteLine($"{km:F2}") ;
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Programirane/ConsoleApp1/town info/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace town_info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameofcity = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            int square = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {nameofcity} has population of {population} and area {square}square km");
+
+            
+        }
+    }
+}
