ff65c167b3782bc9f4318ad4397700fdbe10af70
kristiqn <ksdfiw@gmail.com>
2023-03-01 12:20:25 +0200

    g

---
+++ Programirane/task 2/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int total = 0;
+            while (n != 0) ;
+            {
+                total += n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
