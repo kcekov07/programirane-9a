2b051e8865d2c24407a556564f233a6c6a4c09a9
kristiqn <ksdfiw@gmail.com>
2023-03-22 14:01:14 +0200

    add

--- Programirane/metodi i clasove/metodi i clasove/Program.cs
+++ Programirane/metodi i clasove/metodi i clasove/Program.cs
@@ -29,78 +29,32 @@ namespace metodi_i_clasove
 
                 if (options == "1")
                 {
-                    Console.WriteLine(GetSum(a, b));
+                    Console.WriteLine(Class1.GetSum(a, b));
                 }
                 else if (options == "2")
                 {
-                    Console.WriteLine(GetSumofallNumbers(a, b));
+                    Console.WriteLine(Class1.GetSumofallNumbers(a, b));
                 }
                 else if (options == "3")
                 {
-                    int[] result = MakeArrayofNumsBetween(a, b);
+                    int[] result = Class1.MakeArrayofNumsBetween(a, b);
                     Console.WriteLine(string.Join(",", result));
                 }
                 else if (options == "4")
                 {
-                    PrintOddNUmbersBetween(a, b);
+                    Class1.PrintOddNUmbersBetween(a, b);
 
                 }
                 else if (options == "5")
                 {
-                    PrintEvenNumsBetween(a, b);
+                    Class1.PrintEvenNumsBetween(a, b);
                 }
 
             }
-                Console.ReadLine();
+
         }
 
 
-
-        static int GetSum(int a, int b)
-        {
-
-            int result = a + b;
-            return result;
-        }
-        static int GetSumofallNumbers(int a, int b)
-        {
-            int sum = 0;
-            for (int i = a + 1; i < b; i++)
-            {
-                sum += i;
-            }
-
-            return sum;
-        }
-        static int[] MakeArrayofNumsBetween(int a, int b)
-        {
-            int[] array = new int[b - a + 1];
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = a + i;
-            }
-
-            return array;
-        }
-        static void PrintOddNUmbersBetween(int a, int b)
-        {
-            for (int i = a; i < b; i++)
-            {
-                if(i % 2 != 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-        }
-        static void PrintEvenNumsBetween(int a, int b)
-        {
-            for (int i = a; i < b; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-        }
     }
+       
 }
