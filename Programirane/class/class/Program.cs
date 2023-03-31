f62d56b9e7fd7a99dbaf7e605138b5f2b2100055
kristiqn <ksdfiw@gmail.com>
2023-03-31 12:52:45 +0300

    clasove

---
+++ Programirane/class/class/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1= new Person();
+            p1.Name = "Asen";
+            p1.Age = 16;
+
+            Person p2 = new Person();
+            p2.Name = "nikola";
+            p2.Age = 18;
+
+            Person p3= new Person();
+            p3.Name = "nikola";
+            p3.Age = 18;
+
+            p1.AgeUp();
+            p1.AgeUp();
+
+            Person[] people = {p1,p2,p3};
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (people[i].Age<18)
+                {
+                people[i].AgeUp();
+                }
+            }
+            p1.RecordMyId("dwdwdwdw");
+            p1.PrintMyId();
+
+            Console.ReadLine();
+        }
+    }
+}
+public class Person
+{
+    private string Id = "";
+    public string Name { get; set; }
+    public int Age { get; set; } = 10;
+
+    public void AgeUp ()
+    {
+        Age++;
+        Console.WriteLine($"{Name} aged up and now is {Age} years old");
+    }
+    public void PrintMyId()
+    {
+        Console.WriteLine("*****"+Id.Substring(3,4)+"*****");
+    }
+    public void RecordMyId(string id)
+    {
+       Id = id;
+    }
+}
--- Programirane/metodi i clasove/metodi i clasove/Class1.cs
+++ Programirane/metodi i clasove/metodi i clasove/Class1.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace metodi_i_clasove
 {
-    internal class Class1
+    public class Class1
     {
+        public static int GetSum(int a, int b)
+        {
+
+            int result = a + b;
+            return result;
+        }
+         public static int GetSumofallNumbers(int a, int b)
+        {
+            int sum = 0;
+            for (int i = a + 1; i < b; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+        public static int[] MakeArrayofNumsBetween(int a, int b)
+        {
+            int[] array = new int[b - a + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = a + i;
+            }
+
+            return array;
+        }
+        public static void PrintOddNUmbersBetween(int a, int b)
+        {
+            for (int i = a; i < b; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public static void PrintEvenNumsBetween(int a, int b)
+        {
+            for (int i = a; i < b; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
     }
 }
+
