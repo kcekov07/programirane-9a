6f78a3309bab583c0dad1b03defe1de0ff648b95
kristiqn <ksdfiw@gmail.com>
2023-05-10 14:09:55 +0300

    add

---
+++ Programirane/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var numbers= input.Split(' ').Select(x=>double.Parse(x)).ToList();
+            var result = new Dictionary<double, int>();
+
+            foreach (var num in numbers) 
+            {
+                if (result.ContainsKey(num))
+                {
+                    result[num] += 1;
+                }
+                else
+                {
+                    result[num] = 1;
+                }              
+                           }
+
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value} ");
+            }
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
