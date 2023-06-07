348b40065a9c86a54579729fd848b82885a93fab
kristiqn <ksdfiw@gmail.com>
2023-06-07 10:57:05 +0300

    add

---
+++ Programirane/kursovi zadachi.2/kursovi zadachi.2/zadacha 1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kursovi_zadachi._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string:");
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> charCounts = CountCharacters(input);
+
+            Console.WriteLine("Character Counts (excluding spaces):");
+            foreach (KeyValuePair<char, int> kvp in charCounts)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+            Console.ReadLine();
+        }
+
+        static Dictionary<char, int> CountCharacters(string input)
+        {
+            Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (c != ' ')
+                {
+                    if (charCounts.ContainsKey(c))
+                    {
+                        charCounts[c]++;
+                    }
+                    else
+                    {
+                        charCounts[c] = 1;
+                    }
+                }
+            }
+
+            return charCounts;
+            Console.ReadLine( );
+        }
+
+       
+    }
+   
+}
---
+++ Programirane/kursovi zadachi.2/zadacha 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string resource = Console.ReadLine();
+                if (resource == "stop")
+                {
+                    break;
+                }
+
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resources.ContainsKey(resource))
+                {
+                    resources[resource] += quantity;
+                }
+                else
+                {
+                    resources[resource] = quantity;
+                }
+            }
+
+            foreach (var kvp in resources)
+            {
+                string resource = kvp.Key;
+                int quantity = kvp.Value;
+                Console.WriteLine($"{resource} -> {quantity}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
