5a1e5d2091de3ef5c9345221ef6ed8b7ca53193c
kcekov3 <kcekov3@gmail.com>
2023-06-07 21:29:15 +0300

    add

---
+++ Programirane/kursovi zadachi.2/Zadacha 4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_4
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "buy")
+            {
+                string[] productData = input.Split();
+                string name = productData[0];
+                decimal price = decimal.Parse(productData[1]);
+                int quantity = int.Parse(productData[2]);
+
+                if (products.ContainsKey(name))
+                {
+                    Product product = products[name];
+                    product.Price = price;
+                    product.Quantity += quantity;
+                }
+                else
+                {
+                    Product product = new Product
+                    {
+                        Name = name,
+                        Price = price,
+                        Quantity = quantity
+                    };
+                    products[name] = product;
+                }
+            }
+
+            foreach (var product in products.Values)
+            {
+                decimal totalPrice = product.Price * product.Quantity;
+                Console.WriteLine($"{product.Name} -> {totalPrice:f2}");
+            }
+            Console.ReadLine();
+        }
+
+        class Product
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+        }
+    }
+}
---
+++ Programirane/kursovi zadachi.2/zadacha 3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> keyMaterials = new Dictionary<string, int>()
+        {
+            { "shards", 0 },
+            { "fragments", 0 },
+            { "motes", 0 }
+        };
+
+            SortedDictionary<string, int> junkItems = new SortedDictionary<string, int>();
+
+            string obtainedItem = "";
+
+            while (obtainedItem == "")
+            {
+                string[] input = Console.ReadLine().Split(' ');
+
+                for (int i = 0; i < input.Length; i += 2)
+                {
+                    int quantity = int.Parse(input[i]);
+                    string material = input[i + 1].ToLower();
+
+                    if (keyMaterials.ContainsKey(material))
+                    {
+                        keyMaterials[material] += quantity;
+
+                        if (keyMaterials[material] >= 250)
+                        {
+                            obtainedItem = material;
+                            keyMaterials[material] -= 250;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (!junkItems.ContainsKey(material))
+                            junkItems[material] = 0;
+
+                        junkItems[material] += quantity;
+                    }
+                }
+            }
+
+            PrintResults(obtainedItem, keyMaterials, junkItems);
+            Console.ReadLine();
+        }
+
+        static void PrintResults(string obtainedItem, Dictionary<string, int> keyMaterials, SortedDictionary<string, int> junkItems)
+        {
+            Console.WriteLine($"{char.ToUpper(obtainedItem[0])}{obtainedItem.Substring(1)} obtained!");
+
+            var remainingMaterials = keyMaterials
+                .OrderByDescending(m => m.Value)
+                .ThenBy(m => m.Key)
+                .ToList();
+
+            foreach (var material in remainingMaterials)
+            {
+                Console.WriteLine($"{material.Key}: {material.Value}");
+            }
+
+            foreach (var item in junkItems)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
