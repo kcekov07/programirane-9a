3376c7617cf2963e40563aefcd2a231693cdf4fc
kristiqn <ksdfiw@gmail.com>
2023-06-09 11:57:20 +0300

    add

---
+++ Programirane/test/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> products1 = new Dictionary<string,int>();
+            products1["book"] = 1;
+            products1["car"] = 1;
+            products1["Printer"] = 1;
+
+            if (!products1.ContainsValue(1))
+            {
+                Console.WriteLine("Not Found");
+            }
+            else
+            {
+                foreach (var kv in products1 )
+                {
+                    if (kv.Value == 1)
+                    {
+                        Console.WriteLine(kv.Key);
+                    }
+                }
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
--- Programirane/test/test/Program.cs
+++ Programirane/test/test/Program.cs
@@ -10,7 +10,23 @@ namespace test
     {
         static void Main(string[] args)
         {
+            var products = new Dictionary<string, int>();
+            products.Add("Printer",3 );
+            products.Add("WashingMashine", 1);
+            products.Add("Books", 4);
+            products.Add("Bottle", 7);
+            products.Add("Car ", 4);
+            products.Add("Quitar", 5);
+            products.Add("Screwdriver", 3);
+            products.Add("Bananas", 16);
+            products.Add("Slingshot", 2);
 
+            foreach(KeyValuePair<string, int> item in products)
+            {
+                Console.WriteLine("Item:{0},Quantity:{1}",item.Key,item.Value);
+            }
+
+            Console.ReadLine();
         }
     }
 }
